1e1497b2c609aac09679be7d931c8f2d58a00f73
zhangxiaofan-star <RUzhangchengwei@163.com>
2026-06-12 19:37:45 +0800

    build: 重新构建站点

--- .bash
+++
@@ -1,4 +0,0 @@
-git init
-git add -A
-git commit -m 'deploy5'
-git push -f git@github.com:zhangxiaofan-star/zhangxiaofan-star.github.io.git master
--- .vuepress/enhanceApp.js
+++
@@ -1,3 +0,0 @@
-export default ({ Vue, options, router, siteData, isServer }) => {
-  // 不再全局注入粒子特效背景
-}
